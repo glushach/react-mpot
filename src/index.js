b567a3896ac73fcdadf9d360f7001aab98ebb515
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2025-01-12 10:52:35 +0200

    start project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import App from './components/app/App';
+
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <button
-      style={{ marginBottom: '100' }}
-      onClick={() => console.log('Click on button')}
-    >
-      Click Button
-    </button>
   </React.StrictMode>
 );
