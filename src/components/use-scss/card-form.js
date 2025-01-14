e3375babf579f0fca218bbdaa6f3790b1f548f47
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2025-01-14 15:43:30 +0200

    use scss preprocessor in react

---
+++ src/components/use-scss/card-form.js
@@ -0,0 +1,17 @@
+import './card-form.scss';
+
+export default function CardForm() {
+  return (
+    <div className="card">
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+        repellendus inventore expedita, officiis neque temporibus nihil ducimus
+        fugiat dolore doloribus iure, velit sapiente! Maxime nesciunt ab
+        expedita, dicta temporibus laboriosam?
+      </p>
+      <a href="https://www.youtube.com/watch?v=KAK-WAb9vow" className="link">
+        Link to go
+      </a>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './components/app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <button
-      style={{ marginBottom: '100' }}
-      onClick={() => console.log('Click on button')}
-    >
-      Click Button
-    </button>
   </React.StrictMode>
 );
